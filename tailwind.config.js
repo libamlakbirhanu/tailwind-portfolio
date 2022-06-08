7ad62348f8e29ec6886172b3b8d1228923062ef2
Libamlak Birhanu <libamlakbirhanu904@gmail.com>
2022-06-08 15:36:25 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      screens: {
+        sm: "480px"
+      }
+    },
+    colors: {
+      "faintBlue": "#AADCEC",
+    },
+    fontFamily: {
+      nunito: ['Nunito', 'sans-serif']
+    }
+  },
+  plugins: [],
+};
