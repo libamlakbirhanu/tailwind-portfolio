8085f818e8e7c1273572af07ab64e4407e880bc3
Libamlak Birhanu <libamlakbirhanu904@gmail.com>
2022-06-13 16:31:23 +0300

    implemented the remaining part of the project including light and dark mode

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 const mobileMenu = document.getElementById("mobileMenu");
 const overlay = document.getElementById("overlay");
+const mLinks = document.getElementsByClassName("mLink");
+const toggler = document.getElementById("toggler");
+const body = document.getElementById("body");
+const togglerIcon = document.getElementById("togglerIcon");
+
+if (body.classList.contains("dark")) {
+  togglerIcon.classList.remove("fa-moon");
+  togglerIcon.classList.add("fa-sun");
+}
+
+for (const item of mLinks) {
+  item.addEventListener("click", collapseMobileMenu);
+}
+
+toggler.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (body.classList.contains("dark")) {
+    togglerIcon.classList.remove("fa-moon");
+    togglerIcon.classList.add("fa-sun");
+  } else {
+    togglerIcon.classList.remove("fa-sun");
+    togglerIcon.classList.add("fa-moon");
+  }
+});
 
 const toggleMobileMenu = () => {
   mobileMenu.style.display = "block";
   overlay.style.display = "block";
 };
 
-const collapseMobileMenu = () => {
+function collapseMobileMenu() {
   mobileMenu.style.display = "none";
   overlay.style.display = "none";
-};
+}
